22c38469f168dc58f23d7baf5e48040f3926fd98
Elisha Sunday <sirlaw179@gmail.com>
2026-03-18 16:05:31 +0000

    Set up a Firebase backend

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -1,9 +1,8 @@
-
 export const firebaseConfig = {
-  apiKey: "AIzaSyB-placeholder-api-key",
-  authDomain: "kourrklys-school.firebaseapp.com",
-  projectId: "kourrklys-school",
-  storageBucket: "kourrklys-school.appspot.com",
-  messagingSenderId: "1234567890",
-  appId: "1:1234567890:web:placeholder-app-id"
+  "projectId": "studio-2396221259-224b6",
+  "appId": "1:879040843528:web:637267d72e4bcef6d566bd",
+  "apiKey": "AIzaSyB0114o7DcmhWEhWC14rSpd5YYsZKFkyK0",
+  "authDomain": "studio-2396221259-224b6.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "879040843528"
 };
